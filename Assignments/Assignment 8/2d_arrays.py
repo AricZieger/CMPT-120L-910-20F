8a5308b8c55e3a080007fe8dd99d918b386e8ee4
AricZieger <70243085+AricZieger@users.noreply.github.com>
2020-11-09 14:16:07 -0800

    Update 2d_arrays.py

--- Assignments/Assignment 8/2d_arrays.py
+++ Assignments/Assignment 8/2d_arrays.py
@@ -9,17 +9,18 @@ def sum_2d_array(two_d_array):
     - If a sub array is empty the sum is 0
     """
     pass
-
-if __name__ == "__main__":
-    two_d_array = [
-        [2, 6, 7, 98, 3, 434, 2, 4, 2],
+rows, cols = (5, 5) 
+arr = [[ [2, 6, 7, 98, 3, 434, 2, 4, 2],
         [-12, 3, 454, 6778, 234, -999, 2543, -2323],
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         [],
         [1000000000000000],
         [1],
         [0]
-        ]
+        ]]*cols]*rows 
+print(arr) 
+if __name__ == "__main__":
+    two_d_array = [
     answers = sum_2d_array(two_d_array)
     
     print(answers)
