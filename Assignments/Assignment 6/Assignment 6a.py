830efbf6bb06e865f4b4ab1019b54aaf55e2d120
AricZieger <70243085+AricZieger@users.noreply.github.com>
2020-12-01 10:29:52 -0800

    assignment 6

---
+++ Assignments/Assignment 6/Assignment 6a.py
@@ -0,0 +1,17 @@
+def leap_year(year):
+    if year % 100 == 0 and year % 400 == 0:
+        return "Is a leap year"
+    elif year % 4 == 0 and not year % 100 == 0:
+        return "Is a leap year"
+    else:
+        return "Not a leap year"
+        
+
+
+if year == x %400:
+    years = [2000, 1994, 1912, 3002, 1700, 1400]
+    answers = []
+    for year in years:
+        answers.append(leap_year(year))
+    
+    print(answers)
